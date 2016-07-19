e99763c495521a48f70fe98d708732513537119b
Anton Kostiuchkov <Anton.Kostiuchkov@teleste.com>
2016-07-19 16:43:14 +0200

    cron expression

--- src/directives.js
+++ src/directives.js
@@ -1,6 +1,20 @@
 (function (app) {
 
+    // app.directive('cronField', cronField);
 
+
+    // function cronField() {
+    //     return {
+    //         restrict: 'A',
+    //         link: link
+    //     };
+
+    //     function link(scope, el, attrs) {
+    //         var inputEl = el[0];
+    //         var startPos = inputEl.selectionStart;
+    //         var endPos = inputEl.selectionEnd;
+    //     }
+    // }
 
 
 
