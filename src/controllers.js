7a23bf4d8c6fc506ce55797a380aeed0ca0c0b0a
Anton Kostiuchkov <Anton.Kostiuchkov@teleste.com>
2016-08-03 15:55:58 +0200

    03/08 3:55

--- src/controllers.js
+++ src/controllers.js
@@ -19,31 +19,33 @@
         ctrl.hours = generateNumbers(24,0);
         ctrl.days = generateNumbers(32, 1);
         ctrl.months = [
-            {id: 0, name: 'January', type: 'winter'},
-            {id: 1, name: 'February', type: 'winter'},
-            {id: 2, name: 'March', type: 'spring'},
-            {id: 3, name: 'April', type: 'spring'},
+            {id: 0, name: 'Jan', type: 'winter'},
+            {id: 1, name: 'Febr', type: 'winter'},
+            {id: 2, name: 'Mar', type: 'spring'},
+            {id: 3, name: 'Apr', type: 'spring'},
             {id: 4, name: 'May', type: 'spring'},
-            {id: 5, name: 'June', type: 'summer'},
-            {id: 6, name: 'July', type: 'summer'},
-            {id: 7, name: 'August', type: 'summer'},
-            {id: 8, name: 'September', type: 'autumn'},
-            {id: 9, name: 'October', type: 'autumn'},
-            {id: 10, name: 'November', type: 'autumn'},
-            {id: 11, name: 'December', type: 'winter'}
+            {id: 5, name: 'Jun', type: 'summer'},
+            {id: 6, name: 'Jul', type: 'summer'},
+            {id: 7, name: 'Aug', type: 'summer'},
+            {id: 8, name: 'Sep', type: 'autumn'},
+            {id: 9, name: 'Oct', type: 'autumn'},
+            {id: 10, name: 'Nov', type: 'autumn'},
+            {id: 11, name: 'Dec', type: 'winter'}
         ];
         ctrl.dow = [
-            {id: 0, name: 'Sunday', type: 'weekend'},
-            {id: 1, name: 'Monday', type: 'weekday'},
-            {id: 2, name: 'Tuesday', type: 'weekday'},
-            {id: 3, name: 'Wednesday', type: 'weekday'},
-            {id: 4, name: 'Thursday', type: 'weekday'},
-            {id: 5, name: 'Friday', type: 'weekday'},
-            {id: 6, name: 'Saturday', type: 'weekend'}
+            {id: 0, name: 'Sun', type: 'weekend'},
+            {id: 1, name: 'Mon', type: 'weekday'},
+            {id: 2, name: 'Tue', type: 'weekday'},
+            {id: 3, name: 'Wed', type: 'weekday'},
+            {id: 4, name: 'Thu', type: 'weekday'},
+            {id: 5, name: 'Fri', type: 'weekday'},
+            {id: 6, name: 'Sat', type: 'weekend'}
         ];
 
         ctrl.dailyRecurrence = 1;
         ctrl.weeklyRecurrence = 1;
+        ctrl.monthlyRecurrence = 1;
+        ctrl.advanced = '* * * * *';
 
         ctrl.saveCron = function () {
             console.log('result', dispatcherService.getCronPiece());
